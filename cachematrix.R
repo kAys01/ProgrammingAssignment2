dae932da343184cf93ae8c0e42ff6274e4e546fd
kevin.ays <kvnays@gmail.com>
2016-10-26 15:53:00 -0400

    Committing changes required by homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## will cache a matrix, allowing 4 different functions to act on this object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL                     
+  
+  set <- function(y) {
+    x <<- y                     
+    m <<- NULL                  
+  }
+  
+  get <- function() x           
+  setinverse <- function(solve) m <<- solve  
+  getinverse <- function() m       
+  
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse) #lefthand side = user given function names / righthand side = the functions
 }
 
 
-## Write a short comment describing this function
+## returns the inverse of a square matrix (only if result is mathematically defined)
+#will recompute the inverse for each new matrix given, otherwise, it will pull the cached inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
